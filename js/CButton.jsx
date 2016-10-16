55344166c421b2829a88ef085a598c4ec5a739fe
Akshay Gohil <akshayggohil@yahoo.co.in>
2016-10-16 15:30:15 +0530

    calculation handler added validation pending

--- js/CButton.jsx
+++ js/CButton.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 
 class CButton extends React.Component {
-    handleButtonClickB(){
-        alert('B');
+
+    constructor(props) {
+        super(props);
+
+        this.changeHandler = this.changeHandler.bind(this);
+    };
+
+    changeHandler(e) {
+        if (typeof this.props.onClick === 'function') {
+            this.props.onClick(this.props.value );
+        }
     }
 
 
     render() {
-
         return (
-            <button onClick={this.handleButtonClickB}>{this.props.text}</button>
+            <button onClick={this.changeHandler}>{this.props.text}</button>
         )
     }
 }
+
+CButton.propTypes= {
+    value:  React.PropTypes.string,
+    onClick:   React.PropTypes.func
+};
 
 export default CButton;
--- js/Calculator.jsx
+++ js/Calculator.jsx
@@ -7,8 +7,35 @@ class Calculator extends React.Component {
         super(props);
 
         this.state = {
+            result: 0,
+            expression: ''
+        };
+
+        this.changeHandler = this.changeHandler.bind(this);
+        this.clearHandler = this.clearHandler.bind(this);
+        this.evalThis = this.evalThis.bind(this);
+    }
+
+    changeHandler(value) {
+        var temp = this.state.expression + value;
+        this.setState({
+            expression: temp
+        });
+    }
+
+    evalThis(){
+        this.setState({
+            result: eval(this.state.expression),
+            expression: ""
+        });
+    }
+
+    clearHandler(){
+        var expr = this.state.expression;
+        this.setState({
+            expression: (expr.substring(0, (expr.length - 1))),
             result: 0
-        }
+        });
     }
 
     render() {
@@ -18,32 +45,32 @@ class Calculator extends React.Component {
                 <table>
                     <tbody>
                     <tr>
-                        <td colSpan="3"><p>{this.state.result}</p></td>
-                        <td><CButton text="DEL"></CButton></td>
+                        <td colSpan="3"><p>{(this.state.expression == "")?this.state.result:this.state.expression}</p></td>
+                        <td><CButton text="DEL" value="DEL" type="opt" onClick={this.clearHandler}></CButton></td>
                     </tr>
                     <tr>
-                        <td><CButton text="7"></CButton></td>
-                        <td><CButton text="8"></CButton></td>
-                        <td><CButton text="9"></CButton></td>
-                        <td><CButton text="&#247;"></CButton></td>
+                        <td><CButton text="7" value="7" type="arg" onClick={this.changeHandler}></CButton></td>
+                        <td><CButton text="8" value="8" type="arg" onClick={this.changeHandler}></CButton></td>
+                        <td><CButton text="9" value="9" type="arg" onClick={this.changeHandler}></CButton></td>
+                        <td><CButton text="&#247;" value="/" type="opt" onClick={this.changeHandler}></CButton></td>
                         </tr>
                     <tr>
-                        <td><CButton text="4"></CButton></td>
-                        <td><CButton text="5"></CButton></td>
-                        <td><CButton text="6"></CButton></td>
-                        <td><CButton text="&#215;"></CButton></td>
+                        <td><CButton text="4" value="4" type="arg" onClick={this.changeHandler}></CButton></td>
+                        <td><CButton text="5" value="5" type="ar g" onClick={this.changeHandler}></CButton></td>
+                        <td><CButton text="6" value="6" type="arg" onClick={this.changeHandler}></CButton></td>
+                        <td><CButton text="&#215;" value="*" type="opt" onClick={this.changeHandler}></CButton></td>
                     </tr>
                     <tr>
-                        <td><CButton text="1"></CButton></td>
-                        <td><CButton text="2"></CButton></td>
-                        <td><CButton text="3"></CButton></td>
-                        <td><CButton text="-"></CButton></td>
+                        <td><CButton text="1" value="1" type="arg" onClick={this.changeHandler}></CButton></td>
+                        <td><CButton text="2" value="2" type="arg" onClick={this.changeHandler}></CButton></td>
+                        <td><CButton text="3" value="3" type="arg" onClick={this.changeHandler}></CButton></td>
+                        <td><CButton text="-" value="-" type="opt" onClick={this.changeHandler}></CButton></td>
                     </tr>
                     <tr>
-                        <td><CButton text="."></CButton></td>
-                        <td><CButton text="0"></CButton></td>
-                        <td><CButton text="="></CButton></td>
-                        <td><CButton text="+"></CButton></td>
+                        <td><CButton text="." value="." type="arg" onClick={this.changeHandler}></CButton></td>
+                        <td><CButton text="0" value="0" type="arg" onClick={this.changeHandler}></CButton></td>
+                        <td><CButton text="=" value="=" type="opt" onClick={this.evalThis}></CButton></td>
+                        <td><CButton text="+" value="+" type="opt" onClick={this.changeHandler}></CButton></td>
                     </tr>
                     </tbody>
                 </table>
